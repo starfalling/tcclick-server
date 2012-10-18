449aa7aa401b7d29f1dcaefd3a1cc70b728ac9fe
York.Gu <gyq5319920@gmail.com>
2012-10-18 21:17:34 +0800

    use memcache to cache network model for performance up

--- protected/models/Network.php
+++ protected/models/Network.php
@@ -14,28 +14,38 @@ class Network{
 		return self::$all_networks;
 	}
 	
-	private static function reload(){
-		self::$all_networks = array();
-		$sql = "select * from {networks}";
-		$stmt = TCClick::app()->db->query($sql);
-		while(true){
-			$row = $stmt->fetch(PDO::FETCH_ASSOC);
-			if(!$row) break;
-			self::$all_networks[$row['network']] = $row['id'];
+	private static function reload($refreshCache=false){
+		if(!$refreshCache){
+			self::$all_networks = TCClick::app()->cache->get('tcclick_all_networks');
+		}else{
+			self::$all_networks = false;
 		}
+		if(self::$all_networks === false){
+			self::$all_networks = array();
+			$sql = "select * from {networks}";
+			$stmt = TCClick::app()->db->query($sql);
+			while(true){
+				$row = $stmt->fetch(PDO::FETCH_ASSOC);
+				if(!$row) break;
+				self::$all_networks[$row['network']] = $row['id'];
+			}
+			TCClick::app()->cache->set('tcclick_all_networks', self::$all_networks);
+		}
+		return self::$all_networks;
 	}
+	
 	/**
 	 * add a network to database by name
 	 * @param string $network
 	 */
 	public static function add($network){
-		$sql = "insert into {networks} (network) values (:network)";
-		if(TCClick::app()->db->execute($sql, array(":network"=>$network))){
-			self::$all_networks[$network] = TCClick::app()->db->lastInsertId();
-		}
+		$sql = "insert ignore into {networks} (network) values (:network)";
+		TCClick::app()->db->execute($sql, array(":network"=>$network));
+		self::reload(true);
 	}
 	
 	/**
+	 * query unique id of the network in database, create one if not exist 
 	 * @param string $network
 	 */
 	public static function idFor($network){
@@ -48,12 +58,14 @@ class Network{
 	}
 	
 	/**
-	 * @return string $name
+	 * query network name by chanel id
+	 * @param integer $id
+	 * @return string
 	 */
-	public static function nameof($id){
-	  foreach (self::all() as $name=>$network_id){
-	    if($network_id == $id) return $name;
-	  }
+	public static function nameOf($id){
+		foreach(self::all() as $name=>$network_id){
+			if($network_id == $id) return $name;
+		}
 	}
 }
 
