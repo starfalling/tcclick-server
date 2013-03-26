907ea57836d4a614cbcb714fe34db90498b67b8e
York.Gu <gyq5319920@gmail.com>
2013-03-26 15:18:24 +0800

    加入cron任务以删除过早的用来记录活跃设备列表的table以减少数据库空间的占用

---
+++ api/drop_old_hourly_active_devices_tables.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * 删除过早的用来存储小时活跃设备ID列表的table，以释放数据库空间
+ */
+
+include dirname(dirname(__FILE__)) . '/protected/init.php';
+include_once TCClick::app()->root_path . '/protected/components/RegPattern.php';
+
+
+$today = strtotime(date('Y-m-d'));
+$from_time = $today-TCCLICK_HOURLY_ACTIVE_DEVICE_RECORD_TIME-86400;
+$to_time = $today-TCCLICK_HOURLY_ACTIVE_DEVICE_RECORD_TIME;
+if(isset($_GET['from']) && preg_match(RegPattern::DATE, $_GET['from'])){
+	$time = strtotime($_GET['from']);
+	if($time < $from_time) $from_time = $time;
+}
+
+// 逐天删除这些表
+for($time=$from_time; $time<$to_time; $time+=86400){
+	$table_name = "hourly_active_devices_" . date('Y_m_d', $time);
+	$sql = "drop table {{$table_name}}";
+	echo $sql, "\n";
+	TCClick::app()->db->execute($sql);
+}
+
+
