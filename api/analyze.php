fe88b6097d5d888c1584227af7697e6bdd9ee357
York.Gu <gyq5319920@gmail.com>
2016-08-12 18:30:24 +0800

    analyze fix

--- api/analyze.php
+++ api/analyze.php
@@ -9,24 +9,22 @@ $dbMigrateUtil->upgrade();
 define("KEY_LOADING_CLIENT_ACTIVIES_LOCK", "KEY_LOADING_CLIENT_ACTIVIES_LOCK");
 
 $script_start_time = time();
-while(true) {
-  $result = TCClick::app()->cache->add(KEY_LOADING_CLIENT_ACTIVIES_LOCK, "locked", 5);
-  if($result) break;
-  if(time() - $script_start_time > 1) exit;
-  usleep(10 * 1000);
-}
 
-$sql = "select * from {client_activities} order by id limit 1000";
-$stmt = TCClick::app()->db->query($sql);
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-if(!empty($rows)) {
-  $max_id = $rows[count($rows) - 1]['id'];
+while(true) {
+  if(time() - $script_start_time > 60) exit;
+
+  $sql = "select * from {client_activities} order by id limit 1000";
+  $stmt = TCClick::app()->db->query($sql);
+  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  if(!empty($rows)) {
+    $max_id = $rows[count($rows) - 1]['id'];
 // 	$sql = "insert into {client_activities_backup} select * from {client_activities} where id <= $max_id";
 // 	TCClick::app()->db->execute($sql);
-  $sql = "delete from {client_activities} where id <= $max_id";
-  TCClick::app()->db->execute($sql);
-}
-TCClick::app()->cache->delete(KEY_LOADING_CLIENT_ACTIVIES_LOCK);
+    $sql = "delete from {client_activities} where id <= $max_id";
+    TCClick::app()->db->execute($sql);
+  } else {
+    break;
+  }
 
 //$test_data = '{"timestamp":1470909016, "device":{"udid":"d4d1858a723f478b9c51da392d13bce7",' .
 //  '"channel":"googlePlay","model":"SCH-I959","brand":"samsung","os_version":"5.0.1","app_version":"2.0",' .
@@ -41,11 +39,12 @@ TCClick::app()->cache->delete(KEY_LOADING_CLIENT_ACTIVIES_LOCK);
 //  'data_compressed' => gzcompress($test_data),
 //));
 
-foreach($rows as $row) {
-  $data_uncompressed = @gzuncompress($row['data_compressed']);
-  if(!$data_uncompressed) $data_uncompressed = @gzinflate($row['data_compressed']);
-  if(!$data_uncompressed) continue;
+  foreach($rows as $row) {
+    $data_uncompressed = @gzuncompress($row['data_compressed']);
+    if(!$data_uncompressed) $data_uncompressed = @gzinflate($row['data_compressed']);
+    if(!$data_uncompressed) continue;
 
-  $analyzer = new Analyzer($row['server_timestamp'], intval($row['ip']), $data_uncompressed);
-  $analyzer->analyze();
+    $analyzer = new Analyzer($row['server_timestamp'], intval($row['ip']), $data_uncompressed);
+    $analyzer->analyze();
+  }
 }
